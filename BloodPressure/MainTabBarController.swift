feb807e97f265ba716fdd0824fce707c796be45f
Aleksander Maj <aleksander.maj@htdevelopers.com>
2017-12-26 11:41:40 +0100

    Add CareKit do Podfile.

---
+++ BloodPressure/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  BloodPressure
+//
+//  Created by Aleksander Maj on 22/12/2017.
+//  Copyright © 2017 Aleksander Maj. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+}
---
+++ BloodPressure/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  BloodPressure
+//
+//  Created by Aleksander Maj on 22/12/2017.
+//  Copyright © 2017 Aleksander Maj. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
